27a12b666c31a520d3d1b3acee8a0ac9ce7bc5f3
Krishna Kumar Gupta <kumarkrishn663@gmail.com>
2023-02-04 12:30:56 +0530

    backend

---
+++ routes/admin.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const AdminModel = require("../models/admin");
+const adminRouter = express.Router();
+
+adminRouter.get("/",async(req,res)=>{
+    let data;
+    try { 
+         data = await AdminModel.find();
+}
+    catch(error) {
+        console.log(err)
+        return res.status(404).json({message:"something went wrong"})
+    }
+    let singleData = data[data.length -1]
+    res.status(200).json(singleData);
+})
+
+adminRouter.post("/",async(req,res)=>{
+    let data = new AdminModel(req.body);
+    try {
+        await data.save()
+    } catch (error) {
+        return res.status(404).json({message:"data not saved"})
+    }
+    res.status(201).json({message:"Successfully saved"})
+})
+
+
+ 
+
+module.exports = adminRouter;
